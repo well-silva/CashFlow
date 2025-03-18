f84382635e368b2c0e47067268e700d3dd03d313
wells <well.silvaa97@gmail.com>
2025-03-18 04:44:59 -0300

    feat: add testes

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestRegisterExpenseBuilder
+    {
+        public static RequestRegisterExpenseDto Build()
+        {
+            var faker = new Faker();
+
+            return new Faker<RequestRegisterExpenseDto>()
+                .RuleFor(request => request.Title, faker.Commerce.Product())
+                .RuleFor(request => request.Description, faker.Commerce.ProductDescription())
+                .RuleFor(request => request.Amount, faker.Random.Decimal(1, 1000))
+                .RuleFor(request => request.Date, faker.Date.Past())
+                .RuleFor(request => request.PaymentType, faker.PickRandom<PaymentType>());
+        }
+    }
+}
---
+++ tests/Validator.Test/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,107 @@
+﻿using CashFlow.Aplication.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using Shouldly;
+
+namespace Validator.Test.Expenses.Register
+{
+    public class RegisterExpenseValidatorTests
+    {
+        [Fact]
+        public void Success()
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseBuilder.Build();
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.ShouldNotBeNull();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("     ")]
+        [InlineData(null)]
+        public void ErrorTitleEmpty(string title)
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseBuilder.Build();
+            request.Title = title;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.ShouldBeFalse();
+            result.Errors.ShouldSatisfyAllConditions(
+                erro => erro.ShouldHaveSingleItem(),
+                erro => erro[0].ErrorMessage.ShouldBe(ResourceErrorMessages.TITLE_REQUIRED)
+            );
+        }
+
+        [Fact]
+        public void ErrorDateFuture()
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseBuilder.Build();
+            request.Date = DateTime.UtcNow.AddDays(1);
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.ShouldBeFalse();
+            result.Errors.ShouldSatisfyAllConditions(
+                erro => erro.ShouldHaveSingleItem(),
+                erro => erro[0].ErrorMessage.ShouldBe(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE)
+            );
+        }
+
+        [Fact]
+        public void ErrorPaymentTypeInvalid()
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseBuilder.Build();
+            request.PaymentType = (PaymentType)800;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.ShouldBeFalse();
+            result.Errors.ShouldSatisfyAllConditions(
+                erro => erro.ShouldHaveSingleItem(),
+                erro => erro[0].ErrorMessage.ShouldBe(ResourceErrorMessages.PAYMENT_TYPE_INVALID)
+            );
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-10)]
+        public void ErrorAmountInvalid(decimal amount)
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseBuilder.Build();
+            request.Amount = amount;
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.ShouldBeFalse();
+            result.Errors.ShouldSatisfyAllConditions(
+                erro => erro.ShouldHaveSingleItem(),
+                erro => erro[0].ErrorMessage.ShouldBe(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO)
+            );
+        }
+    }
+}
