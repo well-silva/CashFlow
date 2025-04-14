647d79bf917749fc372da2554525e439d7d71d05
wells <well.silvaa97@gmail.com>
2025-04-14 20:31:34 -0300

    Add JWT authentication and enhance security features
    Updated `CashFlow.Api.csproj` to include packages for JWT authentication, EF Core design, and Swagger. Added `Authorize` attribute to `ExpensesController` to enforce authorization. Configured JWT authentication in `Program.cs` with token validation parameters and updated middleware to include authentication and authorization.

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -5,12 +5,14 @@ using CashFlow.Aplication.UseCases.Expenses.Register;
 using CashFlow.Aplication.UseCases.Expenses.Update;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ExpensesController : ControllerBase
     {
         [HttpPost]
--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -3,6 +3,9 @@ using CashFlow.Api.Middlewares;
 using CashFlow.Aplication;
 using CashFlow.Infrastructure;
 using CashFlow.Infrastructure.Migrations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,8 +16,23 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExpectionFilter)));
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddAplication();
-
 builder.Services.AddRouting(builder => builder.LowercaseUrls = true);
+
+var signingKey = builder.Configuration.GetValue<string>("Settings:Jwt:SigningKey");
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config => {
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = new TimeSpan(0),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!)),
+    };
+});
 
 var app = builder.Build();
 
@@ -28,6 +46,7 @@ app.UseMiddleware<CultureMiddleware>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
