fc75bf1ae2449189ae74becb6e2a36909381e81a
wells <well.silvaa97@gmail.com>
2025-04-26 17:23:43 -0300

    fix: swagger bearer token description and auth scheme

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -17,12 +17,12 @@ builder.Services.AddSwaggerGen(config =>
     config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Name = "Authorization",
-        Description = @"JTW Authorization header using the Bearer scheme.
-                      Enter 'Bearer' [space] and then your token in the text input below.     
-                      Example: 'Bearer 123ASDDSA'",
+        Description = @"JWT Authorization header using the Bearer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
         In = ParameterLocation.Header,
         Scheme = "Bearer",
-        Type = SecuritySchemeType.ApiKey,
+        Type = SecuritySchemeType.ApiKey
     });
 
     config.AddSecurityRequirement(new OpenApiSecurityRequirement
@@ -53,15 +53,16 @@ var signingKey = builder.Configuration.GetValue<string>("Settings:Jwt:SigningKey
 
 builder.Services.AddAuthentication(config =>
 {
-    config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(config => {
+}).AddJwtBearer(config =>
+{
     config.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = false,
         ValidateAudience = false,
         ClockSkew = new TimeSpan(0),
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!)),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!))
     };
 });
 
